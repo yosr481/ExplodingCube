eb6bf269ba13f4e7dc0f526bca3aceca1c4ead7d
RIENITZ-PC\Rienitz <yosr481@gmail.com>
2018-04-09 22:22:56 +0700

    Everything has completed. Closes #1. Close #2. Close #3. Close #4.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [HideInInspector]
+    public Color particleColor;
+
+    public GameObject cube;
+    public ParticleSystem particle;
+
+    public bool isDestroyed = false;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (isDestroyed)
+        {
+            particle.startColor = particleColor;
+            particle.Play();
+            StartCoroutine(CreateNewCube());
+            isDestroyed = false;
+        }
 	}
+
+    IEnumerator CreateNewCube()
+    {
+        yield return new WaitForSeconds(.2f);
+
+        particle.Stop();
+
+        yield return new WaitForSeconds(.8f);
+
+        GameObject newCube = Instantiate(cube, transform.position, Quaternion.identity) as GameObject;
+        Camera.main.backgroundColor = Random.ColorHSV();
+    }
 }
