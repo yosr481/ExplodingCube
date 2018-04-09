ad8423fef0fb02f5b3b7196bed65ed0b6a3d1ea1
RIENITZ-PC\Rienitz <yosr481@gmail.com>
2018-04-10 00:30:56 +0700

    Music Player, Ready for Build.

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour
+{
+    static MusicPlayer instance = null;
+
+    private AudioSource music;
+
+    void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player self-destructing!");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+            music = GetComponent<AudioSource>();
+            music.loop = true;
+            music.Play();
+        }
+    }
+}
